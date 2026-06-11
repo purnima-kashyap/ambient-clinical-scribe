cd3072ffa125cb19275925978fb52c01995ac296
Purnima Kashyap <pu.kashyap1921@gmail.com>
2026-06-11 23:02:22 +0530

    Added speaker diarization module

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from backend.diarization.speaker_diarizer import diarize_audio
 
 app = FastAPI()
 
 @app.get("/")
 def home():
     return {"message": "Healthcare AI Scribe Running"}
+
+@app.get("/diarize")
+def diarize():
+    return diarize_audio()
